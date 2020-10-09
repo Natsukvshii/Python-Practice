2409875a60f64e607e760f3b80bc7510489bcb96
Pedro DeJesus <67390669+Natsukvshii@users.noreply.github.com>
2020-10-09 18:19:31 -0400

    Simple addition of two variables

---
+++ simpleAddition.py
@@ -0,0 +1,11 @@
+x=0
+y=0
+sum = 0
+
+x = input("X: ")
+y = input("Y: ")
+# You must chance the data type to int bc the plus sign thinks you are working with strings. 
+sum = int(y) + int(x)
+
+print("Answer is: ")
+print(sum)
